14f3a2c496cb615c68a7f5f54bd7f3f008d672b9
ice1000 <ice1000kotlin@foxmail.com>
2023-11-20 05:17:33 -0500

    init: basic cleanup

--- createmap.py
+++ createmap.py
@@ -1,31 +1,27 @@
 import math, random
 
-n = 6
-mines_num = 4
-mine_map= [[0 for col in range(n)] for row in range(n)]
-
-
-def set_mines():
+def set_mines(mines_num: int = 4, n: int = 6):
+    mine_map = [[0 for col in range(n)] for row in range(n)]
     count_mine = 0
     while count_mine < mines_num:
-        val1 = random.randint(0, 6)
-        row = val1
-        val2 = random.randint(0, 6)
-        col = val2 
-        if (row == 0 and col == 0) or (row == 1 and col == 0) or (row == 0 and col == 0):
+        row = random.randint(0, n)
+        col = random.randint(0, n)
+        if (row == 0 and col == 0) or (row == 1 and col == 0) or (row == 0 and col == 1):
             continue
         else:
             if mine_map[row][col] != -1:
-                count += 1
+                count_mine += 1
                 mine_map[row][col] = -1
-def set_hints():
-    cansolve = False
-    while not cansolve:
+    return mine_map
+
+def set_hints(mine_map: list[list[int]]):
+    can_solve = False
+    while not can_solve:
         for row in range(len(mine_map)):
             for col in range(len(mine_map[0])):
-             pass
+                pass
     
     pass
 
-def set_hintshelper(hint_num = 5):
+def set_hints_helper(hint_num = 5):
     pass
